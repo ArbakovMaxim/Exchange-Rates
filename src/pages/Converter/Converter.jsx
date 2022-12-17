c2946609bdef9693590ab94533360366e1ce0151
Arbakov Maxim <fefaodessa@gmail.com>
2022-12-17 19:38:12 +0200

    Finfish

--- src/pages/Converter/Converter.jsx
+++ src/pages/Converter/Converter.jsx
@@ -1,9 +1,17 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { WrapperConvert } from './Converter.styled';
+import {
+  Input,
+  Option,
+  Select,
+  WrapperConvert,
+  WrapperSelect,
+} from './Converter.styled';
 
 const Converter = () => {
-  const length = ['USD', 'EUR', 'UAH'];
+  const currencies = ['USD', 'EUR', 'UAH'];
+
+  const data = JSON.parse(localStorage.getItem('currencyData'));
 
   const [usdAmound, setUsdAmound] = useState(0);
   const [eurAmound, setEurAmound] = useState(0);
@@ -12,45 +20,45 @@ const Converter = () => {
   const [secondAmound, setSecondAmound] = useState(0);
   const [firstCurrent, setFirstCurrent] = useState('USD');
   const [secondCurrent, setSecondCurrent] = useState('USD');
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
 
   useEffect(() => {
-    const data = JSON.parse(localStorage.getItem('currencyData'));
     setUsdAmound(data[0].rateBuy.toFixed(2));
     setEurAmound(data[1].rateBuy.toFixed(2));
     setUahAmound(1);
-  }, []);
+  }, [data]);
 
   useEffect(() => {
-    if (firstAmound === 0 && firstAmound) {
-      return;
+    if (amountInFromCurrency === true) {
+      if (firstCurrent === 'USD' && secondCurrent === 'USD') {
+        setSecondAmound(firstAmound);
+      }
+      if (firstCurrent === 'USD' && secondCurrent === 'EUR') {
+        setSecondAmound((firstAmound * (usdAmound / eurAmound)).toFixed(2));
+      }
+      if (firstCurrent === 'USD' && secondCurrent === 'UAH') {
+        setSecondAmound((firstAmound * (uahAmound * usdAmound)).toFixed(2));
+      }
+      if (firstCurrent === 'EUR' && secondCurrent === 'EUR') {
+        setSecondAmound(firstAmound);
+      }
+      if (firstCurrent === 'EUR' && secondCurrent === 'USD') {
+        setSecondAmound((firstAmound * (eurAmound / usdAmound)).toFixed(2));
+      }
+      if (firstCurrent === 'EUR' && secondCurrent === 'UAH') {
+        setSecondAmound((firstAmound * (uahAmound * eurAmound)).toFixed(2));
+      }
+      if (firstCurrent === 'UAH' && secondCurrent === 'UAH') {
+        setSecondAmound(firstAmound);
+      }
+      if (firstCurrent === 'UAH' && secondCurrent === 'USD') {
+        setSecondAmound((firstAmound * (uahAmound / usdAmound)).toFixed(2));
+      }
+      if (firstCurrent === 'UAH' && secondCurrent === 'EUR') {
+        setSecondAmound((firstAmound * (uahAmound / eurAmound)).toFixed(2));
+      }
     }
-    if (firstCurrent === 'USD' && secondCurrent === 'USD') {
-      setSecondAmound(firstAmound);
-    }
-    if (firstCurrent === 'USD' && secondCurrent === 'EUR') {
-      setSecondAmound(firstAmound * (usdAmound / eurAmound).toFixed(2));
-    }
-    if (firstCurrent === 'USD' && secondCurrent === 'UAH') {
-      setSecondAmound(firstAmound * (uahAmound * usdAmound).toFixed(2));
-    }
-    if (firstCurrent === 'EUR' && secondCurrent === 'EUR') {
-      setSecondAmound(firstAmound);
-    }
-    if (firstCurrent === 'EUR' && secondCurrent === 'USD') {
-      setSecondAmound(firstAmound * (eurAmound / usdAmound).toFixed(2));
-    }
-    if (firstCurrent === 'EUR' && secondCurrent === 'UAH') {
-      setSecondAmound(firstAmound * (uahAmound * eurAmound).toFixed(2));
-    }
-    if (firstCurrent === 'UAH' && secondCurrent === 'UAH') {
-      setSecondAmound(firstAmound);
-    }
-    if (firstCurrent === 'UAH' && secondCurrent === 'USD') {
-      setSecondAmound(firstAmound * (uahAmound * usdAmound).toFixed(2));
-    }
-    if (firstCurrent === 'UAH' && secondCurrent === 'EUR') {
-      setSecondAmound(firstAmound * (uahAmound * eurAmound).toFixed(2));
-    }
+    return;
   }, [
     eurAmound,
     usdAmound,
@@ -58,37 +66,41 @@ const Converter = () => {
     firstCurrent,
     secondCurrent,
     uahAmound,
+    amountInFromCurrency,
   ]);
 
   useEffect(() => {
-    if (firstCurrent === 'USD' && secondCurrent === 'USD') {
-      setFirstAmound(secondAmound);
-    }
-    if (firstCurrent === 'USD' && secondCurrent === 'EUR') {
-      setFirstAmound(secondAmound * (usdAmound / eurAmound).toFixed(2));
-    }
-    if (firstCurrent === 'USD' && secondCurrent === 'UAH') {
-      setFirstAmound(secondAmound * (uahAmound * usdAmound).toFixed(2));
-    }
-    if (firstCurrent === 'EUR' && secondCurrent === 'EUR') {
-      setFirstAmound(secondAmound);
-    }
-    if (firstCurrent === 'EUR' && secondCurrent === 'USD') {
-      setFirstAmound(secondAmound * (eurAmound / usdAmound).toFixed(2));
-    }
-    if (firstCurrent === 'EUR' && secondCurrent === 'UAH') {
-      setFirstAmound(secondAmound * (uahAmound * eurAmound).toFixed(2));
-    }
-    if (firstCurrent === 'UAH' && secondCurrent === 'UAH') {
-      setFirstAmound(secondAmound);
-    }
-    if (firstCurrent === 'UAH' && secondCurrent === 'USD') {
-      setFirstAmound(secondAmound * (uahAmound * usdAmound).toFixed(2));
-    }
-    if (firstCurrent === 'UAH' && secondCurrent === 'EUR') {
-      setFirstAmound(secondAmound * (uahAmound * eurAmound).toFixed(2));
+    if (amountInFromCurrency === false) {
+      if (firstCurrent === 'USD' && secondCurrent === 'USD') {
+        setFirstAmound(secondAmound);
+      }
+      if (firstCurrent === 'USD' && secondCurrent === 'EUR') {
+        setFirstAmound((secondAmound * (usdAmound / eurAmound)).toFixed(2));
+      }
+      if (firstCurrent === 'USD' && secondCurrent === 'UAH') {
+        setFirstAmound((secondAmound * (uahAmound * usdAmound)).toFixed(2));
+      }
+      if (firstCurrent === 'EUR' && secondCurrent === 'EUR') {
+        setFirstAmound(secondAmound);
+      }
+      if (firstCurrent === 'EUR' && secondCurrent === 'USD') {
+        setFirstAmound((secondAmound * (eurAmound / usdAmound)).toFixed(2));
+      }
+      if (firstCurrent === 'EUR' && secondCurrent === 'UAH') {
+        setFirstAmound((secondAmound * (uahAmound * eurAmound)).toFixed(2));
+      }
+      if (firstCurrent === 'UAH' && secondCurrent === 'UAH') {
+        setFirstAmound(secondAmound);
+      }
+      if (firstCurrent === 'UAH' && secondCurrent === 'USD') {
+        setFirstAmound((secondAmound * (uahAmound * usdAmound)).toFixed(2));
+      }
+      if (firstCurrent === 'UAH' && secondCurrent === 'EUR') {
+        setFirstAmound((secondAmound * (uahAmound * eurAmound)).toFixed(2));
+      }
     }
   }, [
+    amountInFromCurrency,
     eurAmound,
     firstCurrent,
     secondAmound,
@@ -98,54 +110,59 @@ const Converter = () => {
   ]);
 
   function firstCur(e) {
-    setFirstAmound(e.currentTarget.value);
+    const newValue = e.currentTarget.value;
+    setFirstAmound(newValue);
+    setAmountInFromCurrency(true);
   }
 
   function secondCur(e) {
-    setSecondAmound(e.currentTarget.value);
+    const newValue = e.currentTarget.value;
+    setSecondAmound(newValue);
+    setAmountInFromCurrency(false);
   }
 
   return (
     <WrapperConvert>
       <h1>Convert</h1>
-      <div>
-        <input
+      <WrapperSelect>
+        <Input
           type="number"
           name="firstCurrency"
           onChange={firstCur}
           value={firstAmound}
         />
-        <select
+        <Select
           onChange={e => {
             setFirstCurrent(e.target.value);
+            setAmountInFromCurrency(true);
           }}
         >
-          {length.map(option => (
-            <option key={option} value={option}>
+          {currencies.map(option => (
+            <Option key={option} value={option}>
               {option}
-            </option>
+            </Option>
           ))}
-        </select>
-      </div>
-      <div>=</div>
+        </Select>
+      </WrapperSelect>
       <div>
-        <input
+        <Input
           type="number"
           name="secondCurrency"
           onChange={secondCur}
           value={secondAmound}
         />
-        <select
+        <Select
           onChange={e => {
             setSecondCurrent(e.target.value);
+            setAmountInFromCurrency(true);
           }}
         >
-          {length.map(option => (
-            <option key={option} value={option}>
+          {currencies.map(option => (
+            <Option key={option} value={option}>
               {option}
-            </option>
+            </Option>
           ))}
-        </select>
+        </Select>
       </div>
     </WrapperConvert>
   );
--- src/pages/Converter/Converter.styled.js
+++ src/pages/Converter/Converter.styled.js
@@ -3,3 +3,42 @@ import styled from 'styled-components';
 export const WrapperConvert = styled.div`
 margin-top:20px;
 margin-left:20px;`
+
+export const Select = styled.select`
+  height:35px;
+  background: white;
+  color: gray;
+  padding-left: 5px;
+  font-size: 14px;
+  border:none;
+  margin-left: 10px;
+
+`;
+
+export const Option = styled.option`         
+color: black;
+background: white;
+font-weight: small;
+display: flex;
+white-space: pre;
+min-height: 20px;
+padding: 0px 2px 1px;
+`;
+
+export const WrapperSelect = styled.div`
+margin-bottom :20px;`
+
+export const Input = styled.input`
+border: 1px solid #cccccc; 
+border-radius: 3px; 
+-webkit-border-radius: 3px; 
+-moz-border-radius: 3px; 
+-khtml-border-radius: 3px; 
+background: #ffffff !important; 
+outline: none; 
+height: 24px; 
+width: 120px;
+color: black;
+font-size: 11px; 
+font-family: Tahoma; 
+`
